10ff146c76453b60ef7f10fa9a06c98c88c33d59
Kazuki Oishi <oishikazuki@gmail.com>
2021-06-05 05:12:35 +0900

    詳細を取ってくるメソッドを用意

--- app/models/notify_target.rb
+++ app/models/notify_target.rb
@@ -1,4 +1,16 @@
 class NotifyTarget < ApplicationRecord
   belongs_to :admin_user
   has_many :subscriptions, through: :notify_targets_of_subscriptions
+
+  NOTIFY_TYPE_SLACK = 'slack'
+  NOTIFY_TYPES = [NOTIFY_TYPE_SLACK]
+
+  def target_details
+    case notify_type
+    when NOTIFY_TYPE_SLACK then
+      return SlackNotifyTarget.find(target_id)
+    else
+      return nil
+    end
+  end
 end
