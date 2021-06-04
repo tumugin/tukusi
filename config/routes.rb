1fa78b7a6deff79ade9729f8f7e24a303078c4d6
Kazuki Oishi <oishikazuki@gmail.com>
2021-06-05 06:17:17 +0900

    ログイン以外のルートを削る

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :admin_users,
-             path: 'auth',
-             :skip => [:registrations, :passwords, :confirmations, :unlocks]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    devise_for :admin_users,
+               path: 'auth',
+               :skip => [:registrations, :passwords, :confirmations, :unlocks]
+    authenticated :admin_user do
+
+    end
+  end
 end
