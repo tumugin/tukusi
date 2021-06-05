a4b2b093f91da3b460db9dec83964d22728832ff
Kazuki Oishi <oishikazuki@gmail.com>
2021-06-06 03:16:37 +0900

    admin_userモデルを修正

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -5,4 +5,19 @@ class AdminUser < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :confirmable, :lockable, :timeoutable, :trackable,
          :jwt_authenticatable, jwt_revocation_strategy: self
+
+  USER_LEVEL_ADMINISTRATOR = 'administrator'
+  USER_LEVEL_SUPERVISOR = 'supervisor'
+  USER_LEVELS = [
+    USER_LEVEL_ADMINISTRATOR,
+    USER_LEVEL_SUPERVISOR,
+  ]
+
+  def is_user_administrator
+    user_level == USER_LEVEL_ADMINISTRATOR
+  end
+
+  def is_user_supervisor
+    user_level == USER_LEVEL_SUPERVISOR
+  end
 end
