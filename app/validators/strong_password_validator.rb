7919b3e42ebe2fc1f21e10df316bdc0e213aa25e
Kazuki Oishi <oishikazuki@gmail.com>
2021-06-06 07:17:32 +0900

    password validatorの実装ミスを修正

--- app/validators/strong_password_validator.rb
+++ app/validators/strong_password_validator.rb
@@ -1,7 +1,8 @@
 class StrongPasswordValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    unless Devise.password_length.cover?(value)
-      record.errors[attribute] << (
+    unless Devise.password_length.cover?(value.length)
+      record.errors.add(
+        attribute,
         options[:message] ||
           "Password must be at least #{Devise.password_length.min} characters and no more than #{Devise.password_length.max} characters"
       )
