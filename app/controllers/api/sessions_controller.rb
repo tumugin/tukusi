54de951649a40ac97b015a96acb9f3ccb1ffe2a9
Kazuki Oishi <oishikazuki@gmail.com>
2021-06-06 10:17:14 +0900

    Cookie周りを修正

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,6 +1,22 @@
 class Api::SessionsController < Devise::SessionsController
   respond_to :json
+
+  # 無効化しても設定されるSessionのクッキーを強制的に切る
+  after_action -> { request.session_options[:skip] = true }
+
   def create
-    super { |resource| @resource = resource }
+    self.resource = warden.authenticate!(auth_options)
+    render json: { token: current_token }
+  end
+
+  protected def auth_options
+    # session storeを利用しない
+    super.merge(store: false)
+  end
+
+  private
+
+  def current_token
+    request.env['warden-jwt_auth.token']
   end
 end
