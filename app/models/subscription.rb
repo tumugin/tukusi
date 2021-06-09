10acd9a1ce5bb88fb685c14c03f9eb88c6609e9d
Kazuki Oishi <oishikazuki@gmail.com>
2021-06-10 05:16:30 +0900

    scopeを書いた

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,8 +1,16 @@
 class Subscription < ApplicationRecord
   belongs_to :admin_user
   has_many :crawl_logs, dependent: :destroy
+  has_many :latest_crawl_logs, -> { order(id: :desc) }, class_name: 'CrawlLog'
   has_many :notify_targets_of_subscriptions
   has_many :notify_targets, through: :notify_targets_of_subscriptions
+
+  scope :enabled, -> {
+    where(enabled: true)
+  }
+  scope :needs_crawl, -> {
+    joins(:latest_crawl_logs).where('TIME_TO_SEC(DATEDIFF(?, latest_crawl_logs.ended_at)) > subscriptions.check_interval_seconds', Time.current)
+  }
 
   SUBSCRIPTION_TYPE_NOKOGIRI = 'nokogiri'
   SUBSCRIPTION_TYPE_PLAIN = 'plain'
@@ -13,4 +21,8 @@ class Subscription < ApplicationRecord
     SUBSCRIPTION_TYPE_PLAIN,
     SUBSCRIPTION_TYPE_JSON,
   ]
+
+  def latest_crawl_log
+    latest_crawl_logs.take
+  end
 end
