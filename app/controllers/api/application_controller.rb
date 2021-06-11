c847d1b1474de167ce702c8cf49cde56ecc6b304
Kazuki Oishi <oishikazuki@gmail.com>
2021-06-11 23:19:27 +0900

    認証周り修正

--- app/controllers/api/application_controller.rb
+++ app/controllers/api/application_controller.rb
@@ -1,17 +1,23 @@
 class Api::ApplicationController < ActionController::API
+  include ActionController::Cookies
+  include ActionController::RequestForgeryProtection
   # active_model_serializersの挙動をカスタマイズするConcernを入れる
   include Api::PaginationConcern
   # すべてのパラメータをキャメルケースからスネークケースに変換するConcernを入れる
   include Api::CamelCaseParametersConcern
   # Authorizationヘッダが付いているリクエストに関してはDeviseのtrackableを無効化する
   before_action :disable_devise_trackable
-  # Sessionを無効化する
+  # Authorizationヘッダが付いているリクエストはSessionを無効化する
   before_action :disable_session
+  # CSRFトークンのチェックをAuthorizationヘッダが付いていないときのみ行う(Cookie認証用)
+  protect_from_forgery with: :exception, unless: -> { request.authorization }
 
   private
 
   def disable_session
-    request.session_options[:skip] = true
+    if request.authorization
+      request.session_options[:skip] = true
+    end
   end
 
   def disable_devise_trackable
--- app/controllers/api/auth/logins_controller.rb
+++ app/controllers/api/auth/logins_controller.rb
@@ -2,8 +2,9 @@ class Api::Auth::LoginsController < Api::ApplicationController
   prepend_before_action :allow_params_authentication!, only: :create
 
   def create
+    sign_out scope
     resource = warden.authenticate!(auth_options)
-    sign_in(:admin_user, resource)
+    sign_in(scope, resource)
     token = Warden::JWTAuth::UserEncoder
               .new
               .call(current_admin_user, scope, nil)
@@ -14,7 +15,7 @@ class Api::Auth::LoginsController < Api::ApplicationController
   private
 
   def auth_options
-    { scope: scope, store: false }
+    { scope: scope, store: true }
   end
 
   def scope
