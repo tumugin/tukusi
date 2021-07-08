f093bad7fca67003d692917c8a5ab5a66e3eb7c7
Kazuki Oishi <oishikazuki@gmail.com>
2021-07-09 03:23:34 +0900

    productionモードで起動出来なかったので修正

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ require "active_job/railtie"
 require "action_cable/engine"
 require "rails/test_unit/railtie"
 require "sprockets/railtie"
+require "action_mailer/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
