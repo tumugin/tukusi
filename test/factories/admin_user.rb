bf2680b82a5398e40fff96cd0e836cf226076d41
Kazuki Oishi <oishikazuki@gmail.com>
2021-08-24 05:01:13 +0900

    テストを追加

---
+++ test/factories/admin_user.rb
@@ -0,0 +1,16 @@
+FactoryBot.define do
+  factory :admin_user do
+    name { '汐入あすか' }
+    email { 'asuka@example.com' }
+    password { 'test_password' }
+    user_level { AdminUser::USER_LEVEL_ADMINISTRATOR }
+
+    trait :administrator do
+      user_level { AdminUser::USER_LEVEL_ADMINISTRATOR }
+    end
+
+    trait :supervisor do
+      user_level { AdminUser::USER_LEVEL_SUPERVISOR }
+    end
+  end
+end
