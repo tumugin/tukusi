67dd55a2bac804d88f75a2ecb633b07a0162a1a7
Kazuki Oishi <oishikazuki@gmail.com>
2021-10-25 00:28:24 +0900

    camelCaseになってないAPIがあったので修正した

--- app/controllers/api/meta_controller.rb
+++ app/controllers/api/meta_controller.rb
@@ -1,5 +1,7 @@
 class Api::MetaController < Api::ApplicationController
   def show
     render json: { csrf_token: form_authenticity_token }
+      .deep_stringify_keys
+      .deep_transform_keys! { |key| key.camelize(:lower) }
   end
 end
